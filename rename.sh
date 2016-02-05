e172e8306ed5115f3cac4cd036ab4a9d5ede5f3e
Tahiro Hashizume <12ej117@ms.dendai.ac.jp>
2016-02-05 21:45:04 +0900

    progress

---
+++ rename.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for file in `ls rt57i*`
+	do
+	ofilename=${file}
+	nfilename=`cat ${file} | grep write | sed -e s/\\(//g -e s/\\)//g -e s/\\{//g -e s/\\ //g`
+	mv ${ofilename} ${nfilename}.sample
+done
