54c7940f6c987f57e415339df3575c619af0b9a2
Tahiro Hashizume <12ej117@ms.dendai.ac.jp>
2016-02-05 22:41:38 +0900

    progress

--- function.d/rename.sh
+++ function.d/rename.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-for file in `ls *.function`
-	do
-	sed -i s/" "/"***"/g ${file}
+cat ./rtxcommand.txt | while read line; do
+	before=`echo ${line} | awk -F, '{print $1}'`
+	after=`echo ${line} | awk -F, '{print $2}'`
+	for file in `ls *.function`
+		do
+		echo ${file}
+		cat ${file} | sed -e s/${before}/${after}/g
+		echo ""
+		echo ""
+	done	
 done
