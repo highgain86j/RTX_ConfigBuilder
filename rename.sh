d4d70c184517e79005d30d98c8910e03a7636986
Tahiro Hashizume <12ej117@ms.dendai.ac.jp>
2016-02-05 22:22:35 +0900

    progress

--- rename.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for file in `ls *.import`
-	do
-	ofilename=${file}
-	nfilename=`cat ${file} | grep write | sed -e s/\\(//g -e s/\\)//g -e s/\\{//g -e s/\\ //g`
-	mv ${ofilename} ${nfilename}.function
-done
