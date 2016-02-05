ffc2ccad9b4086c2480842ed5d4dba971806c428
Tahiro Hashizume <12ej117@ms.dendai.ac.jp>
2016-02-05 22:00:48 +0900

    progress

--- rename.sh
+++ rename.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for file in `ls *.sample`
+for file in `ls *.import`
 	do
 	ofilename=${file}
 	nfilename=`cat ${file} | grep write | sed -e s/\\(//g -e s/\\)//g -e s/\\{//g -e s/\\ //g`
-	mv ${ofilename} ${nfilename}.import
+	mv ${ofilename} ${nfilename}.function
 done
