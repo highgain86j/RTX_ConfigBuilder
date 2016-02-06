72d9278bde9c198a526d3e75e7881754a4641d70
Tahiro Hashizume <12ej117@ms.dendai.ac.jp>
2016-02-06 09:21:07 +0900

    progress

--- command.d/libcommand.sh
+++ command.d/libcommand.sh
@@ -1,32 +1,40 @@
 #!/bin/bash
 tmpfile=`mktemp`
+unavailstr=_unavail
+
+exten=.command
+known=0_all_known.cmnd
+modeldep=1_modeldep.cmnd
+common=2_rt_rtx-common.cmnd
+
 
 if [ -n "`ls unavail_*`" ]
 	then
 	rm unavail_*
 fi
 
-for file in `ls *.command`
+for file in `ls *${exten}`
 	do
 	cat ${file} >> ${tmpfile}
 done
 
-cat ${tmpfile} | sort | uniq > 0_all_known.cmnd
+cat ${tmpfile} | sort | uniq > ${known}
 
-for file in `ls *.command`
+for file in `ls *${exten}`
 	do
-	diff ${file} 0_all_known.cmnd | grep '^>' | sed -e "s/> //g" > unavail_${file}
+	name=`echo ${file} | awk -F. '{print $1}'`
+	diff ${file} ${known} | grep '^>' | sed -e "s/> //g" > ${name}${unavailstr}${exten}
 done
 
 cp /dev/null ${tmpfile}
 
-for file in `ls unavail_*`
+for file in `ls *${unavailstr}${exten}`
 	do
 	cat ${file} >> ${tmpfile}
 done
 
-cat ${tmpfile} | sort | uniq > 1_modeldep.cmnd
+cat ${tmpfile} | sort | uniq > ${modeldep}
 
-diff 1_modeldep.cmnd 0_all_known.cmnd | grep '^>' | sed -e "s/> //g" > 2_rt_rtx-common.cmnd
+diff ${modeldep} ${known} | grep '^>' | sed -e "s/> //g" > ${common}
 
 rm ${tmpfile}
