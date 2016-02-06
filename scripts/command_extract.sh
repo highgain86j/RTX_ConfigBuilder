529e0b0cc5a454c37c92b1918d5ddddea4c518bc
Tahiro Hashizume <12ej117@ms.dendai.ac.jp>
2016-02-06 16:08:19 +0900

    progress

--- scripts/command_extract.sh
+++ scripts/command_extract.sh
@@ -3,14 +3,22 @@ tmpfile=`mktemp`
 
 zipfile=$1
 
-unzip ${zipfile}
+LF=$(printf '\\\012_')
+LF=${LF%_}
+
+unzip ${zipfile} > /dev/null
 
 cd html
 
 for file in `ls */*.html`
-	do 
-	cat ${file} | grep "var" >> ${tmpfile}
+	do
+	cat ${file} | sed 's|<br/>|'"$LF"'|g' | grep "kwd" | grep "var" \
+	| sed -e 's|<span class="var">|'VAR#'|g' -e 's|<span class="delim">||g' -e 's|</span>||g' -e 's|<span class="kwd">||g' \
+	-e 's|<ul class="simple">||g' -e 's|<li>||g' -e 's|<span class="synph">||g' -e 's|</li>||g' >> ${tmpfile}
 done
 
-cat ${tmpfile} > command.lst
+cat ${tmpfile} > ../command.lst
+cd ..
+rm -rf html/
+rm ${tmpfile}
 
